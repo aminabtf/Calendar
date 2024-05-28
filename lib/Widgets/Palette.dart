dd52ba8654d30421b97e11f43637bb731b59e200
aminabtf <bouterfamina@gmail.com>
2024-05-28 09:45:11 +0200

    first commit

---
+++ lib/Widgets/Palette.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+//this is the color palette of the app9
+//to call a color use Palette."color_name"
+class Palette {
+  static const white = Color.fromARGB(255, 255, 255, 255);
+  static const black = Color.fromARGB(255, 31, 30, 31);
+  static const grey = Color.fromARGB(255, 196, 196, 196);
+  static const lightGrey = Color.fromARGB(255, 232, 232, 232);
+  static const darkGrey = Color.fromARGB(255, 89, 89, 89);
+  static const brown = Color.fromARGB(255, 221, 158, 41);
+  static const red = Color.fromARGB(255, 255, 34, 0);
+  static const lightRed = Color.fromARGB(255, 244, 135, 153);
+  static const orange = Color.fromARGB(255, 255, 123, 0);
+  static const yellow = Color.fromARGB(255, 255, 208, 0);
+  static const pink = Color.fromARGB(255, 246, 55, 118);
+  static const magenta = Color.fromARGB(255, 129, 0, 127);
+  static const purple = Color.fromARGB(255, 177, 0, 208);
+  static const indigo = Color.fromARGB(255, 0, 99, 199);
+  static const blue = Colors.blueAccent;
+  static const lightBlue = Color.fromARGB(255, 54, 180, 254);
+  static const darkBlue = Color.fromARGB(255, 12, 132, 201);
+  static const smaltBlue = Color.fromARGB(255, 75, 142, 142);
+  static const lightsmaltBlue = Color.fromARGB(255, 150, 200, 200);
+  static const apple = Color.fromARGB(255, 57, 156, 61);
+  static const darkGreen = Color.fromARGB(255, 0, 171, 0);
+  static const lightGreen = Color.fromARGB(255, 10, 202, 10);
+  static const lighterGreen = Color.fromARGB(255, 95, 228, 74);
+  static const lighterGreen01 = Color.fromARGB(
+      255, 144, 238, 124); // Adjusted RGB values for a lighter green color
+  static const pastelGreen = Color.fromARGB(
+      255, 190, 225, 215); // Adjusted RGB values for a pastel green color
+
+  static const Mandalay = Color.fromARGB(255, 183, 135, 31);
+}
